4f831160a53b671cd530f6cc5c9b89a3adb124bd
Alex <ianta@ualberta.ca>
2023-12-11 10:25:30 -0700

    update dec 11 2023

--- scrape_mongo.sh
+++ scrape_mongo.sh
@@ -2,6 +2,22 @@
 
 #Usage: ./scrape_mongo.sh <mongo-collection> <es-index>
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+echo "scrape_mongo.sh script located at: $SCRIPT_DIR"
+
+if [ -z "$1" ]
+then
+        echo "No flightId provided."
+        exit -1
+fi
+
+if [ -z "$2" ]
+then
+        echo "No es-index provided."
+        exit -1
+fi
+
 echo "scraping mongo collection: $1 into es-index: $2"
 
 echo "Generating mongodata.conf"
@@ -14,11 +30,16 @@ input {
                 placeholder_db_name => 'logstash_sqlite.db'
                 collection => '$1'
                 batch_size => 5000
+                parse_method => 'dig'
+                dig_fields => ['eventDetails','nodes','paths', 'name', 'action','type','xpath','path','domSnapshot', 'timestamps','eventTimestamp', 'element']
         }
 }
 filter {
         mutate {
-                remove_field => ["_id"]
+                remove_field => ["_id", "log_entry"]
+        }
+        mutate {
+                gsub => ["metadata", "=>", ": "]
         }
 }
 output {
@@ -41,7 +62,7 @@ docker stop logstash || true && docker rm logstash || true
 
 echo "running logstash"
 
-echo "$mongodata" > ./config/mongodata.conf &&\
- docker run -d --network=logui_es-net -v $(pwd)/config/:/usr/share/logstash/config/ -p 9600:9600 aianta/logstash bash -c "logstash -f /usr/share/logstash/config/mongodata.conf"  
+echo "$mongodata" > $SCRIPT_DIR/config/mongodata.conf &&\
+ docker run -d --name=logstash --network=logui_es-net -v $SCRIPT_DIR/config/:/usr/share/logstash/config/ -p 9600:9600 aianta/logstash bash -c "logstash -f /usr/share/logstash/config/mongodata.conf"  
 
 
